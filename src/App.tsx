0ee0f78703d86c5d2b4eb39280cc6bf66f920f6c
Louis <huynguyennbk@gmail.com>
2022-06-05 01:51:39 +0700

    setup(#4): react component chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Badge } from '@chakra-ui/react';
 
 function App() {
-  return <div className="mt-5">Hello World</div>;
+  return (
+    <div className="mt-5">
+      <Badge>Default</Badge>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 // eslint-disable-next-line
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
