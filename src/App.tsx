01fafce2b002eda3bbb12dbf344cfd2b26917747
Louis <huynguyennbk@gmail.com>
2022-06-05 02:08:32 +0700

    setup(#2): react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Badge } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import AppRoutes from './routes';
 
 function App() {
   return (
-    <div className="mt-5">
-      <Badge>Default</Badge>
+    <div>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Badge>Default</Badge>
+          <AppRoutes />
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 // eslint-disable-next-line
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
